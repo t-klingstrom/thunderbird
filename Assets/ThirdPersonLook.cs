ac4462105d97846c43989a906723a3dbcdac4a31
Gwan Yip <gwan@codeandcraft.nyc>
2017-11-13 21:03:14 -0500

    Building initial environment to prototype

---
+++ Assets/ThirdPersonLook.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonLook : MonoBehaviour {
+
+    public float rotateSpeed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        transform.RotateAround(transform.position, Vector3.up, horizontal);
+        transform.RotateAround(transform.position, Vector3.left, vertical);
+    }
+}
